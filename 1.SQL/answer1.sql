adf3a2cb2eaba4e56add2b2150c7b55c00fb8c4c
Bourbon <820425786@qq.com>
2017-10-23 22:36:40 +0800

    1.1 and 1.2

---
+++ 1.SQL/answer1.sql
@@ -0,0 +1,90 @@
+#1
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order`(
+  oid INT(11) NOT NULL  AUTO_INCREMENT,
+  cuid INT(11) NOT NULL ,
+  cid INT(11) NOT NULL ,
+  quantity INT(11) NOT NULL ,
+  totalPrice DOUBLE NOT NULL ,
+  orderTime DATETIME NOT NULL ,
+  PRIMARY KEY (oid)
+);
+
+#2
+SELECT name, price FROM clothes
+WHERE type = '衬衫' AND price >= 120 AND price <=150
+ORDER BY price  ;
+
+#3
+SELECT name, ifnull(sum(quantity),0) FROM
+	(SELECT * FROM clothes
+	WHERE launchYear = 2015 AND brand = 'nike' AND type = '裤子') temp LEFT
+JOIN `order` ON temp.cid = `order`.cid
+GROUP BY temp.cid;
+
+#4
+SELECT sum(totalPrice) as total_custom FROM `order`
+WHERE `order`.cuid = (SELECT cuid FROM customer WHERE cname = 'jacky')
+AND year(orderTime) = 2014 AND month(orderTime) = 11;
+
+#5
+SELECT cname FROM customer WHERE cuid IN (
+	SELECT cuid
+	FROM `order`
+	WHERE cid =
+				(SELECT cid
+				 FROM clothes
+				 WHERE price =
+							 (SELECT max(price)
+								FROM clothes
+								WHERE brand = 'nike' AND launchYear = 2015 AND type = '外套')
+				)
+)
+AND cuid IN (
+	SELECT cuid
+	FROM `order`
+	WHERE cid =
+						(SELECT cid
+						 FROM clothes
+						 WHERE price =
+									 (SELECT max(price)
+										FROM clothes
+										WHERE brand = 'nike' AND launchYear = 2015 AND type = '裤子')
+						)
+);
+
+#6
+SELECT name, brand, sum(quantity) as total_quantity FROM clothes, `order`
+WHERE clothes.cid = `order`.cid
+AND date_format(orderTime, '%Y.%m.%d') = '2014.12.12'
+GROUP BY `order`.cid
+ORDER BY total_quantity DESC LIMIT 3;
+
+#7
+SELECT cname, phone FROM customer
+WHERE cuid =
+			(SELECT cuid FROM `order`
+			WHERE cuid IN
+						(SELECT cuid FROM clothes, `order`
+						WHERE clothes.cid = `order`.cid AND date_format(orderTime, '%Y.%m.%d') = '2014.11.11'  AND brand = 'nike')
+			GROUP BY cuid
+			 ORDER BY sum(totalPrice) DESC LIMIT 1);
+
+#8
+SELECT * FROM customer
+WHERE cuid NOT IN
+			(SELECT cuid FROM `order`
+			WHERE totalPrice <800 AND date_format(orderTime, '%Y.%m.%d') = '2014.12.12')
+AND cuid IN
+		(SELECT cuid FROM `order`
+		WHERE date_format(orderTime, '%Y.%m.%d') = '2014.12.12');
+
+#9
+DELETE FROM customer
+WHERE cuid NOT IN
+			(SELECT cuid FROM `order`
+			WHERE orderTime BETWEEN '2014.9.1' AND '2015.9.1');
+
+#10
+GRANT UPDATE ,INSERT ,SELECT ON customer TO Mike;
+REVOKE DELETE ON customer FROM Mike;
---
+++ 1.SQL/answer2.sql
@@ -0,0 +1,34 @@
+#Work2
+#author Bourbon
+#1
+DELETE FROM commit WHERE total_add > 5000 AND total_delete < 100;
+
+#2
+SELECT id, author, sum(total_add-total_delete) AS totalCode FROM commit, deadline
+WHERE datetime BETWEEN start_day AND end_day
+GROUP BY id, author;
+
+#3
+SELECT java_total/count(DISTINCT filename) AS percent, java_total, count(DISTINCT filename) AS total
+  FROM file,(SELECT COUNT(DISTINCT filename) as java_total
+             FROM file
+             WHERE filename LIKE '%.java') AS j;
+
+#4
+SELECT id, oneday, max(count) FROM
+  (SELECT id, date_format(datetime, '%Y-%m-%d') AS oneday, count(*) AS count FROM commit, deadline
+WHERE date_format(datetime, '%Y-%m-%d') BETWEEN start_day AND end_day
+GROUP BY id, date_format(datetime, '%Y-%m-%d')) AS everyday
+GROUP BY id;
+
+#5
+SELECT filename, sum(add_line-delete_line) AS totalCode FROM file
+WHERE filename LIKE '%.java'
+GROUP BY filename
+HAVING totalCode > 200
+ORDER BY totalCode DESC ;
+
+#6
+UPDATE deadline
+SET end_day = date_sub(end_day, INTERVAL 1 WEEK)
+WHERE id = 3;
